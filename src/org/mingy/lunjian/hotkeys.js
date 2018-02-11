597d2658d7450b058e361c675ade3898d9062493
Mingy <zwu@shaviation.cn>
2018-02-12 02:50:27 +0800

    hongbao

--- src/org/mingy/lunjian/hotkeys.js
+++ src/org/mingy/lunjian/hotkeys.js
@@ -731,7 +731,7 @@ function find_target(nameOrId, types) {
 	}
 	return null;
 }
-var task_h_timer, task_h_listener;
+var task_h_timer, task_h_listener, hongbao_h_listener;
 function stop_task() {
 	if (task_h_timer) {
 		clearInterval(task_h_timer);
@@ -1002,6 +1002,25 @@ function execute_cmd(cmd) {
 				
 			}
 		});
+	} else if (cmd == '#t+ hongbao') {
+		if (!hongbao_h_listener) {
+			console.log('open hongbao trigger...');
+			hongbao_h_listener = add_listener('main_msg', '',
+					function(msg) {
+						if (msg.get('ctype') == 'text') {
+							var r = msg.get('msg').match(/hongbao qiang (.+) gn(\d+)/);
+							if (r) {
+								send_cmd('hongbao qiang ' + r[1] + ' gn' + r[2]);
+							}
+						}
+					}, true);
+		}
+	} else if (cmd == '#t- hongbao') {
+		if (hongbao_h_listener) {
+			console.log('close hongbao trigger...');
+			remove_listener(hongbao_h_listener);
+			hongbao_h_listener = undefined;
+		}
 	} else if (cmd.substr(0, 7) == '#alias ') {
 		var alias = $.trim(cmd.substr(7));
 		var key, value, i = alias.indexOf(' ');
